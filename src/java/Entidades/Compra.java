64fa085850b04608cfabc7e7dac2b48552a01c1f
Diego <Conkergox98gmail.com>
2024-01-10 10:17:28 +0100

    Añadido de las ultimas anotaciones

--- src/java/Entidades/Compra.java
+++ src/java/Entidades/Compra.java
@@ -26,6 +26,9 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class Compra implements Serializable {
 
+    /**
+     * Atributos de la clase Compra con sus respectivas anotaciones
+     */
     private static final long serialVersionUID = 1L;
     @EmbeddedId
     private CompraId compraId;
@@ -40,38 +43,83 @@ public class Compra implements Serializable {
     @ManyToOne
     private Entrada entrada;
 
+    /**
+     * Getter de compraID
+     *
+     * @return compraId
+     */
     public CompraId getCompraId() {
         return compraId;
     }
 
+    /**
+     * Setter de compraId
+     *
+     * @param compraId
+     */
     public void setCompraId(CompraId compraId) {
         this.compraId = compraId;
     }
 
+    /**
+     * Getter de fecha_compra
+     *
+     * @return
+     */
     public Date getFecha_compra() {
         return fecha_compra;
     }
 
+    /**
+     * Setter de fecha compra
+     *
+     * @param fecha_compra
+     */
     public void setFecha_compra(Date fecha_compra) {
         this.fecha_compra = fecha_compra;
     }
 
+    /**
+     * Getter de cliente
+     *
+     * @return cliente
+     */
     public Cliente getCliente() {
         return cliente;
     }
 
+    /**
+     * Setter de cliente
+     *
+     * @param cliente
+     */
     public void setCliente(Cliente cliente) {
         this.cliente = cliente;
     }
 
+    /**
+     * Getter de Entrada
+     *
+     * @return entrada
+     */
     public Entrada getEntrada() {
         return entrada;
     }
 
+    /**
+     * Setter de entrada
+     *
+     * @param entrada
+     */
     public void setEntrada(Entrada entrada) {
         this.entrada = entrada;
     }
 
+    /**
+     * Método autogenerado de hashcode
+     *
+     * @return hash
+     */
     @Override
     public int hashCode() {
         int hash = 0;
@@ -79,6 +127,12 @@ public class Compra implements Serializable {
         return hash;
     }
 
+    /**
+     * Método autogenerado de equals
+     *
+     * @param object
+     *
+     */
     @Override
     public boolean equals(Object object) {
         // TODO: Warning - this method won't work in the case the id fields are not set
@@ -92,6 +146,11 @@ public class Compra implements Serializable {
         return true;
     }
 
+    /**
+     * Método autogenerado de toString
+     *
+     * @return Entidades.Compra
+     */
     @Override
     public String toString() {
         return "Entidades.Compra[ id=" + fecha_compra + " ]";
